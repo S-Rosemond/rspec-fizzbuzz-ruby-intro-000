2b9b31b94e551ad050445a05e9cf2a214fa1b684
Samuel Jean-Rosemond <js.rosemond@gmail.com>
2019-05-15 00:05:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-return "#{number % 3 == 0? "fizz": nil}#{number%5 == 0?"buzz" : nil}"
+return "#{number % 3 == 0? "Fizz": nil}#{number%5 == 0?"Buzz" : nil}"
 end
